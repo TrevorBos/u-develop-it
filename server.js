6abad097eb611ee2d69c7fbbea85ab9f1af6a013
Trevor Bos <trevor.bos@outlook.com>
2022-07-17 15:43:09 -0400

    Server connected to the mysql database election

--- server.js
+++ server.js
@@ -2,14 +2,26 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to the database (mysql)
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Bashi1996",
+    database: "election",
+  },
+  console.log("Connected to the database `election`")
+);
+
 // Default response for any other request (not found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 // Start listening
